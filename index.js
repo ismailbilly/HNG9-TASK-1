a2f1c820b629b13e66927b372dc868185ba98e80
Bilqisu Ismail <ismail.bilkisu@gmail.com>
2022-10-28 18:12:46 +0100

    Added port variable

--- index.js
+++ index.js
@@ -2,16 +2,17 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 app.use(express.json());
+const port = process.env.PORT || 3000;
 // A odject containing user details
 const userDetails = {
     "slackUsername": "Bilkisu Ismail",
     "backend": true,
     "age": 32,
-    "bio": "I am a computer engineering graduate"
+    "bio": "I am a backend engineer"
 };
 //endpoint to display the user details
 app.get('/', (req,res)=>{
     res.status(200).json(userDetails)
 });
 // listening to server
-app.listen(process.env.PORT, ()=>console.log('Server Running'));
+app.listen(port, ()=>console.log('Server Running'));
