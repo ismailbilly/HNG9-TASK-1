1a67fef99465a0e854c65994804bab3221826ab2
Bilqisu Ismail <ismail.bilkisu@gmail.com>
2022-11-05 16:19:34 +0100

    refactored index.js

--- index.js
+++ index.js
@@ -1,45 +1,76 @@
-require('dotenv').config();
 const express = require('express');
 const {mathOperations} = require('./Helpers/operations')
 const {userDetails} = require('./Helpers/userDetails')
 
 const app = express();
+
 app.use(express.json());
+
 app.use(express.urlencoded({extended:false}));
-const port = process.env.PORT || 3000;
 
-// let mathOp,firstOperand,secondOperand
-
-// app.post('/', (req,res)=>{
-//    const {operation_type,x,y} =req.body
-//    mathOp = operation_type
-//    firstOperand = x
-//    secondOperand = y
-// })
-// app.get('/', (req,res)=>{
-//      const result = mathOperations(mathOp,firstOperand,secondOperand)
-//     res.status(200).json({
-//         "slackUsername": "Bilkisu Ismail",
-//         "result": result,
-//         "operation_type":`${mathOp}`
-//     })
-//  })
- app.post('/api/calculate', (req,res)=>{
-   const {operation_type,x,y} =req.body
-   const result = mathOperations(operation_type,x,y)
-    res.status(200).json({
-        "slackUsername": "ismail.bilkisu",
-        "result": result,
-        "operation_type":`${operation_type}`
-    })
- })
- 
+const PORT = process.env.PORT || 3000;
 
 
-//endpoint to display the user details
-// app.get('/', (req,res)=>{
-//     res.status(200).json(userDetails)
-//  });
+app.post('/', (req,res)=>{
+   
+   const {operation_type,x,y} = req.body;
+   
+   let mathOp = null;
+   
+   firstOperand = parseInt(x);
+
+   secondOperand = parseInt(y);  
+
+   let operationResult = 0;
+
+   let matchedOperation = '';
+
+   const possibleOperations = ['add', 'addition', 'sum', 'subtraction', 'difference', 'subtract', 'multiplication', 'product', 'multiply'];
+
+   // Here we inspect the operation_type string in order to know what sort of operation to perform
+   for(let possibleOperation of possibleOperations){
+
+      // This checks to see if a possible operation is found as a substring or matches exactly the operation_type.  
+      if( operation_type.indexOf(possibleOperation) > -1 ){
+          
+         switch(possibleOperation){
+            case 'add':
+            case 'sum':
+            case 'addition':
+                  operationResult = firstOperand + secondOperand;
+                  matchedOperation = 'addition';
+               break;
+
+            case 'subtract':
+            case 'subtraction':
+            case 'difference':
+                  operationResult = firstOperand - secondOperand;
+                  matchedOperation = 'subtraction';
+               break;
+            
+            case 'product':
+            case 'multiplication':
+            case 'multiply':
+                  operationResult = firstOperand * secondOperand;
+                  matchedOperation = 'multiplication';
+               break;
+         }
+
+         // This implies a match has been found and the mathematical operation performed
+         if(matchedOperation !== ''){
+            break; //exits the loop
+         }
+      }
+
+   }
+
+   // JSON response
+   return res.status(200).json({'operation_type': matchedOperation, 'result': operationResult, 'slackUsername': 'ismail.bilkisu' });
+})
 
 // listening to server
-app.listen(port, ()=>console.log('Server Running'));
+app.listen(PORT, ()=>console.log('Server Running'));
+
+
+
+
