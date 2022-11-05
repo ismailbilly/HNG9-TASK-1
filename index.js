80519ae3850f3833d051ed389ee2cb0c5e714d18
Bilqisu Ismail <ismail.bilkisu@gmail.com>
2022-11-05 15:41:42 +0100

    Removed the get request

--- index.js
+++ index.js
@@ -8,22 +8,33 @@ app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 const port = process.env.PORT || 3000;
 
-let mathOp,firstOperand,secondOperand
+// let mathOp,firstOperand,secondOperand
 
-app.post('/', (req,res)=>{
+// app.post('/', (req,res)=>{
+//    const {operation_type,x,y} =req.body
+//    mathOp = operation_type
+//    firstOperand = x
+//    secondOperand = y
+// })
+// app.get('/', (req,res)=>{
+//      const result = mathOperations(mathOp,firstOperand,secondOperand)
+//     res.status(200).json({
+//         "slackUsername": "Bilkisu Ismail",
+//         "result": result,
+//         "operation_type":`${mathOp}`
+//     })
+//  })
+ app.post('/', (req,res)=>{
    const {operation_type,x,y} =req.body
-   mathOp = operation_type
-   firstOperand = x
-   secondOperand = y
-})
-app.get('/', (req,res)=>{
-     const result = mathOperations(mathOp,firstOperand,secondOperand)
+   const result = mathOperations(operation_type,x,y)
     res.status(200).json({
         "slackUsername": "Bilkisu Ismail",
         "result": result,
-        "operation_type":`${mathOp}`
+        "operation_type":`${operation_type}`
     })
  })
+ 
+
 
 //endpoint to display the user details
 // app.get('/', (req,res)=>{
